4759cb8c7631c77b2e0836e82d70b9df61395c69
tsing <tsing@jianqing.org>
2009-11-20 22:03:25 +0800

    make the struct more elegant

--- lib/xmppproxy/xmpp_protocol.py
+++ lib/xmppproxy/xmpp_protocol.py
@@ -3,8 +3,7 @@
 from __future__ import with_statement
  
 import time
-
-import struct
+import random
  
 from twisted.python import log
 from twisted.internet import protocol, reactor, threads
@@ -15,80 +14,85 @@ from twisted.words.protocols.jabber.xmlstream import IQ
 from wokkel.xmppim import MessageProtocol, PresenceClientProtocol
 from wokkel.xmppim import Presence, AvailablePresence
 
-def translate_jid(jid, component):
-    return jid.replace("@", "_at_").replace(".", "_")+"@"+component
-
-def untranslate_jid(jid, component):
-    return jid.replace("@"+component, "").replace("_at_", "@").replace("_", ".")
-
-class XmppProxyMessageProtocol(MessageProtocol):
+class XmppProxyHandler(object):
 
     def __init__(self, component, jid_act_as, jid_proxy_to):
-        super(XmppProxyMessageProtocol, self).__init__()
         self.component = component
-        self.jid_act_as = JID(jid_act_as).full()
-        self.jid_proxy_to = JID(jid_proxy_to).userhost()
+        self.jid_act_as = jid_act_as
+        self.jid_proxy_to = jid_proxy_to
 
-    def connectionInitialized(self):
-        super(XmppProxyMessageProtocol, self).connectionInitialized()
-        log.msg("Connected!")
+    def translate_jid(self, jid):
+        jid = jid.replace("@", "_at_").replace(".", "_")
+        if jid.find("/") > 0:
+            jid = jid.replace("/", "@" + self.component + "/")
+        else:
+            jid = jid + "@" + self.component
+        return jid
 
-    def connectionLost(self, reason):
-        log.msg("Disconnected!")
+    def untranslate_jid(self, jid):
+        return jid.replace("@" + self.component, "").replace("_at_", "@").replace("_", ".")
 
-
-    def onMessage(self, msg):
-        log.msg("chat body message: %s" % msg.toXml())
-        self.forward_msg(msg)
-
-    def forward_msg(self, msg):
-        entity_from = JID(msg["from"])
-        entity_to = JID(msg["to"])
+    def get_forward_jids(self, stanza):
+        entity_from = JID(stanza["from"])
+        entity_to = JID(stanza["to"])
 
         if entity_from.userhost() == self.jid_proxy_to:
-            new_jid_to = untranslate_jid(entity_to.userhost(), self.component)
+            new_jid_to = self.untranslate_jid(entity_to.full())
             new_jid_from = self.jid_act_as
         else:
             new_jid_to = self.jid_proxy_to
-            new_jid_from = translate_jid(entity_from.userhost(), self.component)
+            new_jid_from = self.translate_jid(entity_from.full())
 
-        msg["from"] = new_jid_from
-        msg["to"] = new_jid_to
-        self.send(msg)
+        return new_jid_to, new_jid_from
 
-class XmppProxyPresenceProtocol(PresenceClientProtocol):
+class XmppProxyMessageProtocol(MessageProtocol, XmppProxyHandler):
 
-    def __init__(self, component, jid_act_as, jid_proxy_to) :
-        super(XmppProxyPresenceProtocol, self).__init__()
-        self.component = component
-        self.jid_act_as = JID(jid_act_as).full()
-        self.jid_proxy_to = JID(jid_proxy_to).userhost()
+    def __init__(self, component, jid_act_as, jid_proxy_to):
+        MessageProtocol.__init__(self)
+        XmppProxyHandler.__init__(self, component, jid_act_as, jid_proxy_to)
+
+    def onMessage(self, message):
+        self.forward_message(message)
+
+    def forward_message(self, message):
+        new_jid_to, new_jid_from = self.get_forward_jids(message)
+
+        message["from"] = new_jid_from
+        message["to"] = new_jid_to
+
+        self.send(message)
+
+class XmppProxyPresenceProtocol(PresenceClientProtocol, XmppProxyHandler):
+
+    def __init__(self, component, jid_act_as, jid_proxy_to):
+        PresenceClientProtocol.__init__(self)
+        XmppProxyHandler.__init__(self, component, jid_act_as, jid_proxy_to)
 
     def _onPresence(self, presence):
         self.forward_presence(presence)
 
+    def probe(self, jid_to, jid_from):
+        presence = Presence(JID(jid_to), "probe")
+        presence["from"] = jid_from
+        self.send(presence)
+
     def forward_presence(self, presence):
         type = presence.getAttribute("type", "available")
         if type == "error":
-            log.msg("got error presence: " % presence.toXml())
             return
 
-        entity_from = JID(presence["from"]);
-        entity_to = JID(presence["to"]);
+        entity_from = JID(presence["from"])
+        entity_to = JID(presence["to"])
 
-        if entity_from.userhost() == self.jid_proxy_to:
-            new_jid_to = untranslate_jid(entity_to.userhost(), self.component)
-            new_jid_from = self.jid_act_as
-        else:
-            new_jid_to = self.jid_proxy_to
-            new_jid_from = translate_jid(entity_from.userhost(), self.component)
+        new_jid_to, new_jid_from = self.get_forward_jids(presence)
 
-        presence["from"] = new_jid_from
         presence["to"] = new_jid_to
+        presence["from"] = new_jid_from
+
         self.send(presence)
 
-        if type == "unavailable" or type == "subscribed":
-            new_presence = Presence(JID(entity_from.userhost()), "probe")
-            new_presence["from"] = entity_to.userhost()
-            self.send(new_presence)
+        if type == "subscribed":
+            self.probe(entity_from.full(), entity_to.userhost())
 
+        if type == "available" and entity_from.userhost() != self.jid_proxy_to:
+            self.probe(self.jid_proxy_to, new_jid_from)
